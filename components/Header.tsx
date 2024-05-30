19314a95d56ac4ad470ea9e42db4de1dc67f98a9
deadmanShadow <raihanshamil33@gmail.com>
2024-05-30 20:00:03 +0600

    add

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { MdMenu } from "react-icons/md";
+import MobileNav from "./MobileNav";
 import Navbar from "./Navbar";
 
 const Header = () => {
+  const [headerActive, setHeaderActive] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    // detect scroll
+    const handleScroll = () => {
+      setHeaderActive(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="fixed left-0 right-0 w-full">
-      <div className="container mx-auto">
-        <Navbar containerStyles="bg-red-300 py-12 flex gap-4" />
+    <header
+      className={`fixed max-w-[1920px] top-0 w-full bg-primary-200 ${
+        headerActive ? "h-[100px]" : "h-[124px]"
+      } transition-all z-50`}
+    >
+      <div className="container mx-auto h-full flex items-center justify-between">
+        <Link href="">
+          {/* logo dosent work */}
+          <Image src={"/public/logo.jpg"} width={117} height={55} alt="" />
+        </Link>
+        {/* mobile */}
+        <MobileNav
+          containerStyles={`${headerActive ? "top-[90px]" : "top-[124px]"} ${
+            openNav
+              ? "max-h-max pt-8 pb-10 border-t border-white/10"
+              : "max-h-0 pt-0 overflow-hidden border-white/0"
+          } flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 text-base uppercase font-medium text-white transition-all xl:hidden`}
+        />
+        {/* desktop */}
+        <Navbar containerStyles="text-white flex gap-4 hidden xl:flex" />
+        {/* hide/open menu button */}
+        <button
+          onClick={() => setOpenNav(!openNav)}
+          className="text-white xl:hidden"
+        >
+          <MdMenu className="text-4xl" />
+        </button>
       </div>
     </header>
   );
