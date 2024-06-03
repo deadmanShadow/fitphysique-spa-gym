e73c87ebf8e43615bdf3a1c80be732654c1096c3
deadmanShadow <raihanshamil33@gmail.com>
2024-06-03 20:24:26 +0600

    embership

--- components/MembershipSlider.tsx
+++ components/MembershipSlider.tsx
@@ -6,6 +6,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import CustomButton from "./CustomButton";
 
 // membership data
 const memberShipsData = [
@@ -30,6 +32,66 @@ const memberShipsData = [
         text: "Monday-Friday gym access",
       },
       {
+        icon: MdClose,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenitites",
+      },
+    ],
+  },
+  {
+    title: "Ultimate",
+    price: "45",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Include membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Access to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenitites",
+      },
+    ],
+  },
+  {
+    title: "Professional",
+    price: "60",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Include membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Access to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
         icon: FaCheck,
         text: "Full access to everything",
       },
@@ -43,9 +105,58 @@ const memberShipsData = [
 
 const MembershipSlider = () => {
   return (
-    <Swiper>
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
+    <Swiper
+      slidesPerView={1}
+      modules={[Pagination]}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+      className="min-h-[680px]"
+    >
+      {memberShipsData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="border border-accent hover:bg-primary-300/80 transition-all duration-300 w-full max-w-sm xl:max-w-none mx-auto">
+              <div className="py-5 px-[60px] border-b border-accent">
+                <h4 className="h4">{item.title}</h4>
+              </div>
+              {/* benefits */}
+              <div className="py-[30px] px-[60px]">
+                <ul className="flex flex-col gap-5 mb-7">
+                  {item.benefits.map((item, index) => {
+                    return (
+                      <li className="flex items-center gap-2" key={index}>
+                        <item.icon className="text-accent text-lg" />
+                        {item.text}
+                      </li>
+                    );
+                  })}
+                </ul>
+                {/* prices */}
+                <p className="text-accent mb-8 flex gap-1 items-center">
+                  <sub className="text-4xl">$</sub>
+                  <strong className="text-6xl">{item.price}</strong>
+                  <em className="self-end text-2xl">/month</em>
+                </p>
+                <CustomButton
+                  containerStyles="w-[196px] h-[62px]"
+                  text="Buy Now"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
