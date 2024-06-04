b5201dceca66910f9259320e54dc3f6c5d143f17
deadmanShadow <raihanshamil33@gmail.com>
2024-06-04 21:07:48 +0600

    fixed issues

--- components/SwiperNavButtons.tsx
+++ components/SwiperNavButtons.tsx
@@ -1,26 +1,29 @@
 "use client";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 import { useSwiper } from "swiper/react";
+
 const SwiperNavButtons = ({
-  containerStyles,
-  btnStyles,
-  iconStyles,
-}: {
-  containerStyles: string;
-  btnStyles: string;
-  iconStyles: string;
+  containerStyles = "",
+  btnStyles = "",
+  iconStyles = "",
 }) => {
   const swiper = useSwiper();
+
   return (
-    <div className={`${containerStyles}`}>
-      <button className={`${btnStyles}`} onClick={() => swiper.slidePrev()}>
+    <div className={`${containerStyles} swiper-nav-buttons`}>
+      <button
+        className={`${btnStyles} swiper-nav-button prev-button mt-8`}
+        onClick={() => swiper.slidePrev()}
+      >
         <PiCaretLeftBold className={`${iconStyles}`} />
       </button>
-      <button className={`${btnStyles}`} onClick={() => swiper.slideNext()}>
+      <button
+        className={`${btnStyles} swiper-nav-button next-button mt-8`}
+        onClick={() => swiper.slideNext()}
+      >
         <PiCaretRightBold className={`${iconStyles}`} />
       </button>
     </div>
   );
 };
-
 export default SwiperNavButtons;
